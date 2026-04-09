13ef317a08b166616eb9ab66577d8adea91f0efe
yau-wd <yau.ong@workday.com>
2026-04-09 20:53:48 +0800

    chore(components/jest.config.js): remove test directory

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>/nodes', '<rootDir>/src', '<rootDir>/test'],
+    roots: ['<rootDir>/nodes', '<rootDir>/src'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
